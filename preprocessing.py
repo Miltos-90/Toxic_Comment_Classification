b1d13b8bfa1d197017fe931cc284be2ac53a8d12
Miltos-90 <35740991+Miltos-90@users.noreply.github.com>
2023-01-20 11:32:22 +0100

    Add files via upload

--- preprocessing.py
+++ preprocessing.py
@@ -1,126 +1,46 @@
 """ This module implements the text preprocessing pipeline. 
     It defines all the necessary functions, classes, and objects, and implements all
     the necessary preprocessing steps in the preprocess() function at the bottom.
-
-    Some functions used here come from: https://www.kaggle.com/code/sudalairajkumar/getting-started-with-text-preprocessing
 """
 
 import re
-import string
 import pkg_resources as pkgr
-from num2words import num2words
 from symspellpy import SymSpell
 from unidecode import unidecode
 import contractions
 from typing import Union
-
+from collections import defaultdict
 from nltk.corpus import wordnet
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk import pos_tag
-
 import constants as c
 
-class abbreviations:
 
-    @staticmethod
-    def to_text(text:str, abbrvDict:  dict = c.ABBREVIATIONS) -> str:
-        ''' Converts chat (slang) abbreviatons to standard text. '''
+class regexProcessor(object):
+    ''' Generic class to replace or remove a set of regex patterns'''
+
+    def __init__(self,
+        rDict : dict,            # dictionary containing (keys: regexes, values: replacement texts)
+        flags : re.RegexFlag = 0 # ASCII flags for compilation (https://docs.python.org/3/library/re.html#re.ASCII)
+    ):
+        ''' Compiles the keys of the input dictionary containing the abbreviations and their replacement text.'''
+
+        self.dict = defaultdict()
+
+        for key, value in rDict.items():
+            self.dict[re.compile(key, flags = flags)] = value
+        
+        return
+
+    def transform(self, text:str) -> str:
+        ''' Converts all patterns(keys) to their corresponding values '''
     
-        for abbreviation, meaning in abbrvDict.items():
-            text = re.sub(abbreviation, meaning, text, flags = re.IGNORECASE)
-
-        return text
-
-class emojis:
-
-    @staticmethod
-    def to_text(text:str, emojiDict:dict = c.EMOJIS) -> str:
-        ''' Maps emojis to text. '''
-        
-        for emot in emojiDict:
-            text = re.sub(u'('+emot+')', emojiDict[emot], text)
+        for key, value in self.dict.items():
+            text = key.sub(value, text)
         
         return text
 
-    @staticmethod
-    def remove(text:str, pattern = c.patterns.EMOJI) -> str:
-        ''' Removes Emojis from a string '''
-        return pattern.sub(r'', text)
-
-class emoticons:
-
-    @staticmethod
-    def remove(text:str, pattern = c.patterns.EMOTICON) -> str:
-        ''' Removes Emoticons from a string '''
-        return pattern.sub(r'', text)
-
-    @staticmethod
-    def to_text(text:str, emoticonDict:dict = c.EMOTICONS) -> str:
-        ''' Maps emoticons to text '''
-        
-        for emot in emoticonDict:
-            text = re.sub(u'('+emot+')', emoticonDict[emot], text)
-            
-        return text
-
-class html:
-
-    @staticmethod
-    def remove(text:str, pattern = c.patterns.HTML) -> str:
-        ''' Removes URLs from a string '''
-        return pattern.sub(r'', text)
-
-class url:
-
-    @staticmethod
-    def remove(text:str, pattern = c.patterns.URL) -> str:
-        ''' Removes URLs from a string '''
-        return pattern.sub(r'', text)
-
-class numeric:
-
-    @staticmethod
-    def to_text(text:str) -> str:
-        ''' Maps numbers to text (eg 84 -> eighty-four) '''
-        
-        return re.sub(r'(\d+)', lambda x: num2words(x.group()), text)
-
-class punctuation:
-
-    @staticmethod
-    def remove(text:str, pattern = c.patterns.PUNCTUATION) -> str:
-        ''' Removes punctuation from a string '''
-        return pattern.sub(r'', text)
-
-class stopwords:
-
-    @staticmethod    
-    def remove(text:str, wordList:set = c.STOPWORDS) -> str:
-        ''' Removes stopwords from a string '''
-        tokens = [t for t in text.split() if t not in wordList]
-        return " ".join(tokens)
-
-class spelling:
-
-    @staticmethod
-    def deduplicate(text:str, pattern = c.patterns.DUPLICATE_CHARS) -> str:
-        """ Removes multiple consecutive sequences of consecutive duplicate characters in a string. 
-            eg: cooool -> cool, goooaaal -> goal
-        """
-        return pattern.sub(r"\1\2", text)
-
-    @staticmethod
-    def standardize_lettercase(text:str, pattern = c.patterns.UPPERCASE) -> str:
-        """ Naively standardizes lettercase: Capitalizes the first non-space character on the given string, 
-            and the first character after '.', '!', '?'. Everything else is lowercased.
-        """
-
-        text =  re.sub(pattern,
-                    lambda c: c.group(0).upper(), # Uppercase matches
-                    text.lower())                 # Fully lowercased text
-
-        return text
 
 class SpellingCorrector():
     """ Convenience wrapper for spelling correction with SymSpell
@@ -129,9 +49,9 @@ class SpellingCorrector():
     
     def __init__(self, 
         unigram_txt = "frequency_dictionary_en_82_765.txt",
-        bigram_txt  = "frequency_bigramdictionary_en_243_342.txt"):
-        """ Initialisation method. Loads the necessary dicts
-        """
+        bigram_txt  = "frequency_bigramdictionary_en_243_342.txt"
+    ):
+        """ Initialisation method. Loads the necessary dicts """
         
         self.sp = SymSpell(max_dictionary_edit_distance = 2, prefix_length = 7)
         
@@ -147,23 +67,25 @@ class SpellingCorrector():
     
     
     def __call__(self, text : Union[str, list], max_edit_distance: int = 2, **kwargs):
-        """ Corrector for a single word (text of type str) or a list of words
-        """
+        """ Corrector for a single word (text of type str) or a list of words """
         
-        if isinstance(text, list): return [self._correct(w, max_edit_distance, **kwargs) for w in word_tokenize(text)]
-        else:                      return  self._correct(text, max_edit_distance, **kwargs)
+        if isinstance(text, list): 
+            return [self._correct(w, max_edit_distance, **kwargs) for w in word_tokenize(text)]
+
+        else:                      
+            return self._correct(text, max_edit_distance, **kwargs)
             
 
     def _correct(self, text:str, max_edit_distance: int = 2, **kwargs):
-        """ Convenience wrapper of the lookup_compound command
-        """
+        """ Convenience wrapper of the lookup_compound command """
+
         suggestions = self.sp.lookup_compound(text, max_edit_distance, **kwargs)
 
         return suggestions[0].term
 
+
 class Lemmatizer():
-    ''' Convenience wrapper of the wordnet lemmatizer
-    '''
+    ''' Convenience wrapper of the wordnet lemmatizer '''
 
     def __init__(self):
 
@@ -172,17 +94,15 @@ class Lemmatizer():
         return 
 
     def __call__(self, text: Union[str, list]):
-        ''' Lemmatizes a sentence or list of sentences <text> using the wordnet lemmatizer
-        '''
+        ''' Lemmatizes a sentence or list of sentences <text> using the wordnet lemmatizer '''
         
         if   isinstance(text, list): return [self._lemmatize(sentence) for sentence in text]
         elif isinstance(text, str):  return self._lemmatize(text)
-        else: raise TypeError("Only text of type str or list is supported.")
+        else:                        raise TypeError("Only text of type str or list is supported.")
 
 
     def _lemmatize(self, text:str):
-        ''' Lemmatizes a sentence <text> using the wordnet lemmatizer.
-        '''
+        ''' Lemmatizes a sentence <text> using the wordnet lemmatizer. '''
 
         tokens = word_tokenize(text)
         tags   = [(word, self._penn2morphy(tag)) for word, tag in pos_tag(tokens)]
@@ -205,29 +125,42 @@ class Lemmatizer():
         elif treebankTag.startswith('V'):  return wordnet.VERB
         else:                              return None
 
-# Instantiate spell correction and lemmatizer objects for function-like use 
-lemmatize = Lemmatizer()
-correct   = SpellingCorrector()
+
+""" Instantiate all objects once upon calling the module """
+html          = regexProcessor(rDict = c.HTML,          flags = re.I)
+url           = regexProcessor(rDict = c.URL_STRIP,     flags = re.I)
+abbreviations = regexProcessor(rDict = c.ABBREVIATIONS, flags = re.I)
+emoticons     = regexProcessor(rDict = c.EMOTICONS,     flags = re.UNICODE)
+emojis        = regexProcessor(rDict = c.EMOJIS,        flags = re.UNICODE)
+numeric       = regexProcessor(rDict = c.NUMERIC)
+punctuation   = regexProcessor(rDict = c.PUNCTUATION)
+stopwords     = regexProcessor(rDict = c.STOPWORDS,     flags = re.I)
+duplicates    = regexProcessor(rDict = c.DUPE_CHARS,    flags = re.I)
+lettercase    = regexProcessor(rDict = c.LETTERCASE)
+multispace    = regexProcessor(rDict = c.MULTISPACE)
+lemmatize     = Lemmatizer()
+correct       = SpellingCorrector()
+
 
 def preprocess(text:str) -> str:
     ''' Main processing function '''
 
-    text = url.remove(text)                           # Remove URLs
-    text = html.remove(text)                          # Remove HTML tags
-    text = emoticons.to_text(text)                    # Convert emoticons to words
-    text = emojis.to_text(text)                       # Convert emojis to words
-    text = abbreviations.to_text(text)                # Convert slang abbreviations
-    text = contractions.fix(text)                     # Expand contractions
-    text = numeric.to_text(text)                      # Convert digits to words
-    text = unidecode(text)                            # Convert accented characters
-    text = spelling.standardize_lettercase(text)      # Standardize upper- and lower-case characters
-    text = spelling.deduplicate(text)                 # Remove consecutive multiple instance of duplicated chars 
-    text = sent_tokenize(text, language = 'english')  # Split into sentences
-    text = lemmatize(text)                            # Lemmatize
-    text = [s.lower().strip() for s in text]          # Lower-case and strip leading / trailing spaces
-    text = [punctuation.remove(s) for s in text]      # Remove punctuation
-    text = [stopwords.remove(s) for s in text]        # Remove stopwords
-    text = [correct(s) for s in text]                 # Correct misspelled words
-    text = ' '.join(text)                             # Join sentences
-
-    return text
+    text = html.transform(text)                      # Remove HTML tags
+    text = url.transform(text)                       # Remove URLs
+    text = emoticons.transform(text)                 # Convert emoticons to words
+    text = emojis.transform(text)                    # Convert emojis to words
+    text = abbreviations.transform(text)             # Convert slang abbreviations
+    text = contractions.fix(text)                    # Expand contractions
+    text = numeric.transform(text)                   # Convert digits to words
+    text = unidecode(text)                           # Convert accented characters
+    text = lettercase.transform(text)                # Standardize upper- and lower-case characters
+    text = duplicates.transform(text)                # Remove consecutive multiple instance of duplicated chars 
+    text = sent_tokenize(text, language = 'english') # Split into sentences
+    text = lemmatize(text)                           # Lemmatize
+    text = [s.lower() for s in text]                 # Lower-case
+    text = [punctuation.transform(s) for s in text]  # Remove punctuation
+    text = [correct(s) for s in text]                # Correct misspelled words
+    text = [stopwords.transform(s) for s in text]    # Remove stopwords
+    text = ' '.join(text)                            # Join sentences 
+    text = multispace.transform(text)                # Remove multiple consecutive spaces
+    return text.strip()                              # Remove leading/trailing whitespace
